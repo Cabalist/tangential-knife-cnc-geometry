f1a9129299da4c09ebe0c0e96994c8d79f9877ab
Ryan Jarvis <828557+Cabalist@users.noreply.github.com>
2026-09-11 22:31:16 -0700

    Start of cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -1,40 +1,27 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
-import sys
+"""Sphinx configuration for the geom2d documentation."""
 
-sys.path.append(os.path.abspath('../src'))
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'utl-geom2d'
-copyright = '2024, Claude Zervas'
-author = 'Claude Zervas'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+project = "geom2d"
+copyright = "2026, Ryan Jarvis"  # noqa: A001
+author = "Ryan Jarvis"
 
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "rebuild-plan.md"]
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-#html_theme = 'agogo' #'python_docs_theme'
-#html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
-html_theme = 'utl_sphinx_theme'
-html_theme_options = {
-    'title': 'utl-geom2d',
+autosummary_generate = True
+autodoc_typehints = "description"
+autodoc_member_order = "bysource"
+autodoc_type_aliases = {
+    "PointLike": "geom2d.point.PointLike",
+    "Path": "geom2d.segment.Path",
 }
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
+html_theme = "furo"
+html_title = "geom2d"
